8f6fbceaf925cb0b94fe1a38af5384270925271e
jezstdio <76942168+jezstdio@users.noreply.github.com>
2022-10-22 16:51:36 +0200

    new votes added

--- client/src/vote.js
+++ client/src/vote.js
@@ -289,5 +289,427 @@ export const votes = [
             "end": [1666693790938],
             "repeat": -1
         }
+    },
+
+
+
+
+
+
+    {
+        "_id": "santavsjesus",
+        "ad": {},
+        "published": true,
+        "category": ["Culture"],
+        "type": "public",
+        "teams": {
+            "red": {
+                "name": "Santa",
+                "color": "#F30C47",
+                "scores": [1],
+                "voters": [1]
+            },
+            "blue": {
+                "name": "Jesus",
+                "color": "#006CEF",
+                "scores": [1],
+                "voters": [1]
+            }
+        },
+        "times": {
+            "begin": [1669878000000],
+            "end": [1672527599000],
+            "repeat": -1
+        }
+    },
+    {
+        "_id": "grinchvsxmash",
+        "ad": {},
+        "published": true,
+        "category": ["Culture"],
+        "type": "public",
+        "teams": {
+            "red": {
+                "name": "The Grinch",
+                "color": "#F30C47",
+                "scores": [1],
+                "voters": [1]
+            },
+            "blue": {
+                "name": "Xmash",
+                "color": "#006CEF",
+                "scores": [1],
+                "voters": [1]
+            }
+        },
+        "times": {
+            "begin": [1671901200000],
+            "end": [1672095599000],
+            "repeat": -1
+        }
+    },
+    {
+        "_id": "auchanvstesco",
+        "ad": {},
+        "published": true,
+        "category": ["Food", "Fashion & Brands"],
+        "type": "public",
+        "teams": {
+            "red": {
+                "name": "Auchan",
+                "color": "#F30C47",
+                "scores": [1],
+                "voters": [1]
+            },
+            "blue": {
+                "name": "Tesco",
+                "color": "#006CEF",
+                "scores": [1],
+                "voters": [1]
+            }
+        },
+        "times": {
+            "begin": [1666594800000],
+            "end": [1667170799000],
+            "repeat": -1
+        }
+    },
+    {
+        "_id": "emagvsalza",
+        "ad": {},
+        "published": true,
+        "category": ["Lifestyle", "Fashion & Brands"],
+        "type": "public",
+        "teams": {
+            "red": {
+                "name": "eMag",
+                "color": "#F30C47",
+                "scores": [1],
+                "voters": [1]
+            },
+            "blue": {
+                "name": "Alza",
+                "color": "#006CEF",
+                "scores": [1],
+                "voters": [1]
+            }
+        },
+        "times": {
+            "begin": [1667199600000],
+            "end": [1667775599000],
+            "repeat": -1
+        }
+    },
+    {
+        "_id": "aldivslidle",
+        "ad": {},
+        "published": true,
+        "category": ["Food", "Fashion & Brands"],
+        "type": "public",
+        "teams": {
+            "red": {
+                "name": "Aldi",
+                "color": "#F30C47",
+                "scores": [1],
+                "voters": [1]
+            },
+            "blue": {
+                "name": "Lidl",
+                "color": "#006CEF",
+                "scores": [1],
+                "voters": [1]
+            }
+        },
+        "times": {
+            "begin": [1667804400000],
+            "end": [1668380399000],
+            "repeat": -1
+        }
+    },
+    {
+        "_id": "googlevsmicrosoft",
+        "ad": {},
+        "published": true,
+        "category": ["Tech"],
+        "type": "public",
+        "teams": {
+            "red": {
+                "name": "Google",
+                "color": "#F30C47",
+                "scores": [1],
+                "voters": [1]
+            },
+            "blue": {
+                "name": "Microsoft",
+                "color": "#006CEF",
+                "scores": [1],
+                "voters": [1]
+            }
+        },
+        "times": {
+            "begin": [1668409200000],
+            "end": [1668985199000],
+            "repeat": -1
+        }
+    },
+    {
+        "_id": "chickenvsegg",
+        "ad": {},
+        "published": true,
+        "category": ["Lifestyle", "Culture", "Animals"],
+        "type": "public",
+        "teams": {
+            "red": {
+                "name": "Chicken",
+                "color": "#F30C47",
+                "scores": [1],
+                "voters": [1]
+            },
+            "blue": {
+                "name": "Egg",
+                "color": "#006CEF",
+                "scores": [1],
+                "voters": [1]
+            }
+        },
+        "times": {
+            "begin": [1667286000000],
+            "end": [1698793199000],
+            "repeat": -1
+        }
+    },
+    {
+        "_id": "iosvsandroid",
+        "ad": {},
+        "published": true,
+        "category": ["Tech"],
+        "type": "public",
+        "teams": {
+            "red": {
+                "name": "iOS",
+                "color": "#F30C47",
+                "scores": [1],
+                "voters": [1]
+            },
+            "blue": {
+                "name": "Android",
+                "color": "#006CEF",
+                "scores": [1],
+                "voters": [1]
+            }
+        },
+        "times": {
+            "begin": [1669014000000],
+            "end": [1669589999000],
+            "repeat": -1
+        }
+    },
+    {
+        "_id": "windowsvslinux",
+        "ad": {},
+        "published": true,
+        "category": ["Tech"],
+        "type": "public",
+        "teams": {
+            "red": {
+                "name": "Windows",
+                "color": "#F30C47",
+                "scores": [1],
+                "voters": [1]
+            },
+            "blue": {
+                "name": "Linux",
+                "color": "#006CEF",
+                "scores": [1],
+                "voters": [1]
+            }
+        },
+        "times": {
+            "begin": [1669618800000],
+            "end": [1670194799000],
+            "repeat": -1
+        }
+    },
+    {
+        "_id": "ghostsvswitches",
+        "ad": {},
+        "published": true,
+        "category": ["Culture"],
+        "type": "public",
+        "teams": {
+            "red": {
+                "name": "Ghosts",
+                "color": "#F30C47",
+                "scores": [1],
+                "voters": [1]
+            },
+            "blue": {
+                "name": "Witches",
+                "color": "#006CEF",
+                "scores": [1],
+                "voters": [1]
+            }
+        },
+        "times": {
+            "begin": [1667199600000],
+            "end": [1667429999000],
+            "repeat": -1
+        }
+    },
+    {
+        "_id": "coffeevstea",
+        "ad": {},
+        "published": true,
+        "category": ["Food", "Lifestyle", "Culture"],
+        "type": "public",
+        "teams": {
+            "red": {
+                "name": "Coffee",
+                "color": "#F30C47",
+                "scores": [1],
+                "voters": [1]
+            },
+            "blue": {
+                "name": "Tea",
+                "color": "#006CEF",
+                "scores": [1],
+                "voters": [1]
+            }
+        },
+        "times": {
+            "begin": [1669618800000],
+            "end": [1670194799000],
+            "repeat": -1
+        }
+    },
+    {
+        "_id": "lgvssamsung",
+        "ad": {},
+        "published": true,
+        "category": ["Tech"],
+        "type": "public",
+        "teams": {
+            "red": {
+                "name": "LG",
+                "color": "#F30C47",
+                "scores": [1],
+                "voters": [1]
+            },
+            "blue": {
+                "name": "Samsung",
+                "color": "#006CEF",
+                "scores": [1],
+                "voters": [1]
+            }
+        },
+        "times": {
+            "begin": [1670223600000],
+            "end": [1670799599000],
+            "repeat": -1
+        }
+    },
+    {
+        "_id": "cocacolavspepsi",
+        "ad": {},
+        "published": true,
+        "category": ["Food"],
+        "type": "public",
+        "teams": {
+            "red": {
+                "name": "Coca Cola",
+                "color": "#F30C47",
+                "scores": [1],
+                "voters": [1]
+            },
+            "blue": {
+                "name": "Pepsi",
+                "color": "#006CEF",
+                "scores": [1],
+                "voters": [1]
+            }
+        },
+        "times": {
+            "begin": [1670828400000],
+            "end": [1671404399000],
+            "repeat": -1
+        }
+    },
+    {
+        "_id": "lefthandedvsrighthanded",
+        "ad": {},
+        "published": true,
+        "category": ["Lifestyle", "Culture", "Gaming"],
+        "type": "public",
+        "teams": {
+            "red": {
+                "name": "Left Handed",
+                "color": "#F30C47",
+                "scores": [1],
+                "voters": [1]
+            },
+            "blue": {
+                "name": "Right Handed",
+                "color": "#006CEF",
+                "scores": [1],
+                "voters": [1]
+            }
+        },
+        "times": {
+            "begin": [1671433200000],
+            "end": [1672009199000],
+            "repeat": -1
+        }
+    },
+    {
+        "_id": "digitalvsanalog",
+        "ad": {},
+        "published": true,
+        "category": ["Tech", "Lifestyle"],
+        "type": "public",
+        "teams": {
+            "red": {
+                "name": "Digital",
+                "color": "#F30C47",
+                "scores": [1],
+                "voters": [1]
+            },
+            "blue": {
+                "name": "Analog",
+                "color": "#006CEF",
+                "scores": [1],
+                "voters": [1]
+            }
+        },
+        "times": {
+            "begin": [1672556400000],
+            "end": [1704063599000],
+            "repeat": -1
+        }
+    },
+    {
+        "_id": "redvsblue",
+        "ad": {},
+        "published": true,
+        "category": ["Lifestyle", "Fashion & Brands", "Food", "Tech", "Animals", "Culture", "Gaming"],
+        "type": "public",
+        "teams": {
+            "red": {
+                "name": "Red",
+                "color": "#F30C47",
+                "scores": [1],
+                "voters": [1]
+            },
+            "blue": {
+                "name": "Blue",
+                "color": "#006CEF",
+                "scores": [1],
+                "voters": [1]
+            }
+        },
+        "times": {
+            "begin": [1577862000000],
+            "end": [1924988399000],
+            "repeat": -1
+        }
     }
 ];
